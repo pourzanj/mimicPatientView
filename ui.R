e4454ac228053ca0e505cb1b9bd3099c3fc4106f
Arya Pourzanjani <arya@umail.ucsb.edu>
2016-04-25 14:13:32 -0700

    added extensive comments to ui source

--- ui.R
+++ ui.R
@@ -1,29 +1,37 @@
 library(shiny)
 
-# Load the ggplot2 package which provides
-# the 'mpg' dataset.
-library(ggplot2)
-
 # Define the overall UI
 shinyUI(
   fluidPage(
     titlePanel("MIMIC Patient Viewer"),
     
-    # create new for table
+    # 1. Patient Table -------------------------------------------------
     fluidRow(
       DT::dataTableOutput("mainTable")
     ),
-    
-    #event table
     fluidRow(
+      #once patients are selected this action button is what triggers 
+      #the program to retrieve their recorded events and plot it in the
+      #events table.
       actionButton("getEvents","Retreive Events for Selected Patients")
     ),
+    
+    # 2. Event Table ---------------------------------------------------
     fluidRow(
+      #here we use uiOutput which allows for output formats to change dyanmically.
+      #So we could output text, a table, an image or whatever we need to.
+      #If no patients are selected there should be events and hence no events
+      #table to show. Otherwise we should output a table.
       uiOutput("eventTableUi")
     ),
     
+    # 3. Main Plotting Area --------------------------------------------
     sidebarLayout(
+      # If no patients and events are selected, we don't want anything output here.
+      #Otherwise we do want the plotting options shown.
       uiOutput("plotOptionsUi"),
+      
+      #this is where the main plot of the program actually goes.
       mainPanel(
         plotOutput('mainPlot',width="100%")
       )
